5d019f6ddbcd0606084da62691fa340197dcd9fa
Julian <juliansansat@gmail.com>
2019-06-21 10:02:25 -0300

    added alignment plugin and changed language to pt-br

--- webpack.config.js
+++ webpack.config.js
@@ -14,72 +14,102 @@ const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' 
 const UglifyJsWebpackPlugin = require( 'uglifyjs-webpack-plugin' );
 
 module.exports = {
-	devtool: 'source-map',
-	performance: { hints: false },
+	devtool:
+		'source-map',
+	performance: {
+		hints: false
+	},
 
-	entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
+	entry: path.resolve(
+		__dirname,
+		'src',
+		'ckeditor.js'
+	),
 
 	output: {
 		// The name under which the editor will be exported.
-		library: 'ClassicEditor',
+		library:
+			'ClassicEditor',
 
-		path: path.resolve( __dirname, 'build' ),
-		filename: 'ckeditor.js',
-		libraryTarget: 'umd',
-		libraryExport: 'default'
+		path: path.resolve(
+			__dirname,
+			'build'
+		),
+		filename:
+			'ckeditor.js',
+		libraryTarget:
+			'umd',
+		libraryExport:
+			'default'
 	},
 
 	optimization: {
 		minimizer: [
-			new UglifyJsWebpackPlugin( {
-				sourceMap: true,
-				uglifyOptions: {
-					output: {
-						// Preserve CKEditor 5 license comments.
-						comments: /^!/
+			new UglifyJsWebpackPlugin(
+				{
+					sourceMap: true,
+					uglifyOptions: {
+						output: {
+							// Preserve CKEditor 5 license comments.
+							comments: /^!/
+						}
 					}
 				}
-			} )
+			)
 		]
 	},
 
 	plugins: [
-		new CKEditorWebpackPlugin( {
-			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
-			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
-			language: 'en',
-			additionalLanguages: 'all'
-		} ),
-		new webpack.BannerPlugin( {
-			banner: bundler.getLicenseBanner(),
-			raw: true
-		} )
+		new CKEditorWebpackPlugin(
+			{
+				// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
+				// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
+				language:
+					'pt-br',
+				additionalLanguages:
+					'all'
+			}
+		),
+		new webpack.BannerPlugin(
+			{
+				banner: bundler.getLicenseBanner(),
+				raw: true
+			}
+		)
 	],
 
 	module: {
 		rules: [
 			{
 				test: /\.svg$/,
-				use: [ 'raw-loader' ]
+				use: [
+					'raw-loader'
+				]
 			},
 			{
 				test: /\.css$/,
 				use: [
 					{
-						loader: 'style-loader',
+						loader:
+							'style-loader',
 						options: {
 							singleton: true
 						}
 					},
 					{
-						loader: 'postcss-loader',
-						options: styles.getPostCssConfig( {
-							themeImporter: {
-								themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-							},
-							minify: true
-						} )
-					},
+						loader:
+							'postcss-loader',
+						options: styles.getPostCssConfig(
+							{
+								themeImporter: {
+									themePath: require.resolve(
+										'@ckeditor/ckeditor5-theme-lark'
+									)
+								},
+								minify: true
+							}
+						)
+					}
 				]
 			}
 		]
